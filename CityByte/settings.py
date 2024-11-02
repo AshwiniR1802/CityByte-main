ae902f252f91f11499fc17f472de635542d2cc02
Ashwini Ramanuj <100012749+AshwiniR1802@users.noreply.github.com>
2024-11-02 10:05:33 -0400

    Update settings.py

--- CityByte/settings.py
+++ CityByte/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = [".vercel.app","testserver"]
 
 
 # Application definition
